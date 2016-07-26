42a88ad893d91d48491374875be089a34b2bda78
Dmitriy Kaminskiy <dmitriy.kaminskiy@matrix42.com>
2016-07-26 14:45:52 +0300

    up

--- multipicker/multipicker.js
+++ multipicker/multipicker.js
@@ -8,10 +8,57 @@
 	function MultiPickerController($q) {
 		var vm = this;
 
-		vm.pickerData = [{id: 1, title: 'item 1'}, {id: 2, title: 'item 2'}, {id: 3, title: 'item 3'}, {
+		vm.pickerData = [{
+			id: 1,
+			title: 'SLA00001-Changes Service Level Agreement'
+		}, {
+			id: 2,
+			title: 'SLA00002-Incidents Service Level Agreement'
+		}, {
+			id: 3,
+			title: 'SLA00003-Problems Service Level Agreement'
+		}, {
 			id: 4,
-			title: 'item 4'
-		}, {id: 5, title: 'item 5'}];
+			title: 'SLA04-Support Center Germany (Standard: 8/5)'
+		}, {
+			id: 5,
+			title: 'SLA05-Support Center Americas (Standard: 8/5)'
+		},
+		{
+			id: 6,
+			title: 'SLA06-SMA Solar Technology AG'
+		},
+		{
+			id: 7,
+			title: 'SLA07-R+V Versicherungen AG'
+		},
+		{
+			id: 8,
+			title: 'abc-Ausschluß SetupErrorLog Tests'
+		},
+		{
+			id: 9,
+			title: '42-Premium Support'
+		},
+		{
+			id: 10,
+			title: 'AUS-SB-PremiumPlus-Support Transition Premium Plus'
+		},
+		{
+			id: 11,
+			title: 'AUS-SB-Platinum24x7-Support Transition Platinum 24x7'
+		},
+		{
+			id: 12,
+			title: 'AUS-SB-Premium-Support Transition Premium'
+		},
+		{
+			id: 13,
+			title: 'Support Transition Standard-AUS-SB-Extended-Support Transition Extended'
+		}];
+
+
+
 		vm.pickerArray = ['Item1', 'Item2', 'Item3', 'Item4', 'Item5', 'Item6'];
 		vm.pickerArrayValue = 'Item5';
 		vm.pickerValue = [1, 2];
